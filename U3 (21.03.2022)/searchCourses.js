91404229d0de3e6d3653e0b146058d84f4b349b9
Julia Ermansson <ermanssonj@gmail.com>
2022-03-23 17:39:34 +0100

    render funktion

--- U3 (21.03.2022)/searchCourses.js
+++ U3 (21.03.2022)/searchCourses.js
@@ -4,7 +4,7 @@ function renderCourse (course){
     let div = document.createElement('div')
     div.id = "container"
     div.innerHTML = 
-    `<header>${course.courses[i].title} (${course.courses[i].totalCredits} credits)}</header>
+    `<header>${course.title} (${course.totalCredits} credits)}</header>
     <h4> Course Responsible:     Teachers: </h4>'
     <div id = "teachers">
 
@@ -17,7 +17,7 @@ function renderCourse (course){
 }
 
 function renderCourses (courses) {
-    let songsElement = document.getElementById("container")
+    let coursesElement = document.getElementById("container")
   
     for (let course of courses) {
       let courseElement = renderCourse(course)
--- U3 (21.03.2022)/searchStudents.js
+++ U3 (21.03.2022)/searchStudents.js
@@ -1,23 +1,51 @@
 "use strict"
 
-function renderStudent (student) {
-    let div = document.createElement('div')
+let allStudents = DATABASE.students
+
+function renderStudent (student) {  
+    let div = document.createElement('div');
     div.id = "container"
     div.innerHTML = 
-    `<header>${student.students[i].firstName} ${student.students[i].lastName} (total:  credits)}</header>
-    <h4> Courses </h4>'
+    `<header>${student.firstName} ${student.lastName} (total: ${studentCredits(student)} credits)</header>
+    <h4> Courses </h4>
     <div id = "courses">
 
     </div>
     `
-    console.log(student)
+    return div
   }
 
   function renderStudents (students) {
     let studentsElement = document.getElementById("container")
   
     for (let student of students) {
-      let studentElement = renderSong(student)
+      let studentElement = renderStudent(student)
       studentsElement.appendChild(studentElement)
     }
   }
+
+function studentCredits(){
+  let credits = []
+
+
+  let creditSum = 0;
+  for (let i = 0; i < credits.length; i++){
+    creditSum += credits[i]
+  }
+  return creditSum
+}
+
+function 
+
+renderStudents(allStudents)
+
+function onKeyUp() {
+  console.log(this.value);
+}
+
+
+function searchBar(event){
+  event.preventDefault()
+  let input = document.getElementById("search");
+  input.addEventListener("keyup", onKeyUp);
+}
