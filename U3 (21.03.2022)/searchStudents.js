248e0952d0b9fdb01aa0bce3b33f4147dad3c213
Julia Ermansson <ermanssonj@gmail.com>
2022-03-25 12:08:48 +0100

    funktion AllStudentCourses
    Funktion som hittar alla studenterna och deras kurser

--- U3 (21.03.2022)/searchStudents.js
+++ U3 (21.03.2022)/searchStudents.js
@@ -1,51 +1,78 @@
-"use strict"
+'use strict'
 
 let allStudents = DATABASE.students
+let allCourses = DATABASE.courses
 
-function renderStudent (student) {  
-    let div = document.createElement('div');
-    div.id = "container"
-    div.innerHTML = 
-    `<header>${student.firstName} ${student.lastName} (total: ${studentCredits(student)} credits)</header>
-    <h4> Courses </h4>
+function renderStudent (student) {
+  let div = document.createElement('div')
+  div.id = 'container'
+  div.innerHTML = `<h1>${student.firstName} ${
+    student.lastName
+  } (total: ${studentCredits(student)} credits)</h1>
+    <h2> Courses: </h2>
     <div id = "courses">
-
+    ${allStudentCourses(student)}
     </div>
     `
-    return div
-  }
+  return div
+}
 
-  function renderStudents (students) {
-    let studentsElement = document.getElementById("container")
-  
-    for (let student of students) {
-      let studentElement = renderStudent(student)
-      studentsElement.appendChild(studentElement)
-    }
-  }
+function renderStudents (students) {
+  let studentsElement = document.getElementById('container')
 
-function studentCredits(){
+  for (let student of students) {
+    let studentElement = renderStudent(student)
+    studentsElement.appendChild(studentElement)
+  }
+}
+
+function studentCredits (student) {
   let credits = []
 
+  for (let course of student.courses) {
+    credits.push(course.passedCredits)
+  }
 
-  let creditSum = 0;
-  for (let i = 0; i < credits.length; i++){
+  let creditSum = 0
+  for (let i = 0; i < credits.length; i++) {
     creditSum += credits[i]
   }
   return creditSum
 }
 
-function 
+function allStudentCourses (student) {
+  let theCourses = []
 
-renderStudents(allStudents)
+  for (let i = 0; i < student.courses.length; i++) {
+    let Id = student.courses[i].courseId
+    theCourses.push(allCourses[Id])
+  }
 
-function onKeyUp() {
-  console.log(this.value);
+  let courseArray = []
+
+  for (let i = 0; i < theCourses.length; i++) {
+    let div = document.createElement("div")
+    if(student.courses[i].passedCredits == allCourses[student.courses[i].courseId].totalCredits){
+      let content = div.innerHTML = `
+      <div id = "done"
+      <h4>${allCourses[i].title}</h4> 
+      <p> ${student.courses[i].started.semester} ${student.courses[i].started.year} ( ${student.courses[i].passedCredits} of ${allCourses[student.courses[i].courseId].totalCredits} credits)
+      </div>
+      `
+      courseArray.push(content)
+    } 
+    else {
+      let content = div.innerHTML = `
+      <div id = "notdone"
+      <h4>${allCourses[i].title}</h4> 
+      <p> ${student.courses[i].started.semester} ${student.courses[i].started.year} ( ${student.courses[i].passedCredits} of ${allCourses[student.courses[i].courseId].totalCredits} credits)
+      </div>
+      `
+      courseArray.push(content)
+    }
+    
+  }
+ return courseArray.toString().split(",").join("");
 }
 
 
-function searchBar(event){
-  event.preventDefault()
-  let input = document.getElementById("search");
-  input.addEventListener("keyup", onKeyUp);
-}
